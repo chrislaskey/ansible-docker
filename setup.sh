04f8955250beaa565a14b5cd26713388fb00fd40
Chris Laskey <contact@chrislaskey.com>
2014-11-06 14:24:36 -0500

    Combined setup and run files into one; added sudo requirement

--- setup.sh
+++ setup.sh
@@ -20,6 +20,14 @@ if ! which ansible-galaxy > /dev/null 2>&1; then
     exit 73;
 fi
 
+if ! which ansible-playbook > /dev/null 2>&1; then
+    echo ''
+    echo 'ERROR could not find `ansible-playbook` command.'
+    echo 'Make sure Ansible 1.5+ is installed and `ansible-playbook` is in the shell path.'
+    echo ''
+    exit 74;
+fi
+
 echo ''
 echo "# Installing required ansible playbook:"
 echo ''
@@ -30,6 +38,7 @@ echo ''
 echo '# Successfully set up the ansible environment!'
 echo ''
 echo 'Next steps:'
-echo '1. Update the `hosts` file with the VMs correct ip address'
-echo '2. Execute `run.sh`'
+echo '1. Create a user on the Ubuntu VM with `sudo` permissions'
+echo '2. Update the `hosts` file with the VMs correct ip address'
+echo '3. Execute `ansible-playbook -i hosts -u <remote-user-with-sudo> ./ansible/playbook-docker.yml`'
 echo ''
